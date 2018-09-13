e4b7931334e250dfc0df5a1e262978f470427ea3
oooook0 <yitao.sun@baruchmail.cuny.edu>
2018-09-12 22:36:20 -0400

    update svm

--- src/svm.py
+++ src/svm.py
@@ -55,11 +55,18 @@ class SVM():
             for j in range(0, n):
                 i = self.get_rnd_int(0, n-1, j) # Get random int i~=j
                 x_i, x_j, y_i, y_j = X[i,:], X[j,:], y[i], y[j]
-                k_ij = self.__kernels(x_i, x_i) + self.__kernels(x_j, x_j) - 2 * self.__kernels(x_i, x_j)
-                if k_ij == 0:
-                    continue
+
                 alpha_prime_j, alpha_prime_i = alpha[j], alpha[i]
+
+                s = y_i * y_j
+
                 L, H = self.compute_L_H(self._C, alpha_prime_j, alpha_prime_i, y_j, y_i)
+
+                k_i = self.__kernels(x_i, x_i) 
+                k_j = self.__kernels(x_j, x_j) 
+                k_ij = self.__kernels(x_i, x_j)
+
+                eta = k_i + k_j - 2*k_ij
 
                 # Compute model parameters
                 self.w = self.calc_w(alpha, y, X)
@@ -70,11 +77,40 @@ class SVM():
                 E_i = self.E(x_i, y_i, self.w, self.b)
                 E_j = self.E(x_j, y_j, self.w, self.b)
 
-                # Set new alpha values
-                alpha[j] = alpha_prime_j + float(y_j*(E_i - E_j))/k_ij
-                alpha[j] = max(alpha[j], L)
-                alpha[j] = min(alpha[j], H)
-                alpha[i] = alpha_prime_i + y_i*y_j * (alpha_prime_j - alpha[j])
+                if eta  > 0:
+
+                    # Set new alpha values
+                    alpha[j] = alpha_prime_j + float(y_j*(E_i - E_j))/ eta
+                    alpha[j] = max(alpha[j], L)
+                    alpha[j] = min(alpha[j], H)
+
+                else:
+
+                    f1 = y_i * E_i - alpha_prime_i * k_i - k_ij * alpha_prime_j *s
+                    f2 = y_j * E_j - alpha_prime_j * k_j - k_ij * alpha_prime_i *s
+
+                    L1 = alpha_prime_i + s * (alpha_prime_j - L)
+                    H1 = alpha_prime_i + s * (alpha_prime_j - H)
+
+                    phiL = L1 * f1 + L * f2 + 0.5 * (L1**2) * k_i + 0.5 * (L**2) * k_j + s * L * L1 * k_ij
+                    phiH = H1 * f1 + H * f2 + 0.5 * (H1**2) * k_i + 0.5 * (H**2) * k_j + s * H * H1 * k_ij
+
+                    if phiL < (phiH - self._epsilon):
+                        alpha[j] = L
+                    elif phiL > (phiH + self._epsilon):
+                        alpha[j] = H
+                    else:
+                        pass
+                
+                alpha_prime_i = alpha_prime_i + s * (alpha_prime_j - alpha[j])
+
+                if alpha_prime_i < 0.00001:
+                    alpha_prime_i = 0
+
+                elif alpha_prime_i > self._C - 0.00001:
+                    alpha_prime_i = self._C
+
+                alpha[i] = alpha_prime_i
 
             # Check convergence
             diff = np.linalg.norm(alpha - alpha_prev)
@@ -180,8 +216,8 @@ class Kernel():
 
 if __name__ == "__main__":
 
-    iris = datasets.load_iris()
-    x_vals = np.array([[x[0],x[1],x[2], x[3]] for x in iris.data])
+    iris = datasets.load_breast_cancer()
+    x_vals = np.array(iris.data)
     y_vals = np.array([1 if y == 0 else -1 for y in iris.target])
 
     train_indices = np.random.choice(len(x_vals),
@@ -193,10 +229,8 @@ if __name__ == "__main__":
     y_vals_train = np.transpose([y_vals[train_indices]])
     y_vals_test = np.transpose([y_vals[test_indices]])
 
-    #print(x_vals_test.shape)
-    #print(y_vals_test.shape)
 
-    a = SVM(max_iter=10000, C=1.0, epsilon=0.001, kernels = Kernel.homogenous_polynomial(degree = 2))
+    a = SVM(max_iter=10000, C=1.0, epsilon=0.001, kernels = Kernel.RBF(gamma = 3))
     suppoertV, count = a.fit(X = x_vals_train, y = y_vals_train)
 
     print(suppoertV)
